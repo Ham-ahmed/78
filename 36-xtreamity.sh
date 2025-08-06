7ea44a60509e04b0a8eba10cfe0f7f52b2ee5293
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-08-07 00:05:47 +0300

    Add files via upload

---
+++ 36-xtreamity.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+#remove unnecessary files and folders
+if [  -d "/CONTROL" ]; then
+rm -r  /CONTROL >/dev/null 2>&1
+fi
+rm -rf /control >/dev/null 2>&1
+rm -rf /postinst >/dev/null 2>&1
+rm -rf /preinst >/dev/null 2>&1
+rm -rf /prerm >/dev/null 2>&1
+rm -rf /postrm >/dev/null 2>&1
+rm -rf /tmp/*.ipk >/dev/null 2>&1
+rm -rf /tmp/*.tar.gz >/dev/null 2>&1
+
+#config
+plugin=xtreamity
+version=3.0
+url=https://raw.githubusercontent.com/Ham-ahmed/78/refs/heads/main/36-xstreamity.tar.gz
+package=/var/volatile/tmp/$plugin-$version.tar.gz
+
+#download & install
+echo "> Downloading $plugin-$version package  please wait ..."
+sleep 3s
+
+wget --show-progress -qO $package --no-check-certificate $url
+tar -xzf $package -C /
+extract=$?
+rm -rf $package >/dev/null 2>&1
+
+echo ''
+if [ $extract -eq 0 ]; then
+echo "#########################################################"
+echo "#                INSTALLED SUCCESSFULLY                 #"
+echo "#                    ON - Panel v6                      #"
+echo "#             Enigma2 restart is required               #"
+echo "#        .::UPLOADED BY  >>>>   HAMDY_AHMED::.          #"
+echo "#     https://www.facebook.com/share/g/18qCRuHz26/      #"
+echo "#########################################################"
+echo "#           your Device will RESTART Now                #"
+echo "#########################################################"
+sleep 3s
+
+else
+
+echo "> $plugin-$version package installation failed"
+sleep 3s
+fi
